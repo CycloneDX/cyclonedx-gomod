65fb6198031dbce8e6e7e55434afa536dfeba37e
nscuro <nscuro@protonmail.com>
2021-05-02 18:38:25 +0200

    refactor: reorganize main.go to make it testable
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- main.go
+++ main.go
@@ -14,77 +14,111 @@ import (
 	"github.com/CycloneDX/cyclonedx-gomod/internal/version"
 )
 
-var (
-	componentType   string
-	includeStd      bool
-	modulePath      string
-	noSerialNumber  bool
-	noVersionPrefix bool
-	outputPath      string
-	serialNumber    string
-	showVersion     bool
-	useJSON         bool
-
-	allowedComponentTypes = []cdx.ComponentType{
-		cdx.ComponentTypeApplication,
-		cdx.ComponentTypeContainer,
-		cdx.ComponentTypeDevice,
-		cdx.ComponentTypeFile,
-		cdx.ComponentTypeFirmware,
-		cdx.ComponentTypeFramework,
-		cdx.ComponentTypeLibrary,
-		cdx.ComponentTypeOS,
-	}
-)
+type Options struct {
+	ComponentType    cdx.ComponentType
+	ComponentTypeStr string
+	IncludeStd       bool
+	ModulePath       string
+	NoSerialNumber   bool
+	NoVersionPrefix  bool
+	OutputPath       string
+	SerialNumber     *uuid.UUID
+	SerialNumberStr  string
+	ShowVersion      bool
+	UseJSON          bool
+}
 
 func main() {
-	flag.StringVar(&componentType, "type", string(cdx.ComponentTypeApplication), "Type of the main component")
-	flag.BoolVar(&includeStd, "std", false, "Include Go standard library as component and dependency of the module")
-	flag.StringVar(&modulePath, "module", ".", "Path to Go module")
-	flag.BoolVar(&noSerialNumber, "noserial", false, "Omit serial number")
-	flag.BoolVar(&noVersionPrefix, "novprefix", false, "Omit \"v\" version prefix")
-	flag.StringVar(&outputPath, "output", "-", "Output path")
-	flag.StringVar(&serialNumber, "serial", "", "Serial number (default [random UUID])")
-	flag.BoolVar(&showVersion, "version", false, "Show version")
-	flag.BoolVar(&useJSON, "json", false, "Output in JSON format")
+	var options Options
+
+	flag.StringVar(&options.ComponentTypeStr, "type", string(cdx.ComponentTypeApplication), "Type of the main component")
+	flag.BoolVar(&options.IncludeStd, "std", false, "Include Go standard library as component and dependency of the module")
+	flag.StringVar(&options.ModulePath, "module", ".", "Path to Go module")
+	flag.BoolVar(&options.NoSerialNumber, "noserial", false, "Omit serial number")
+	flag.BoolVar(&options.NoVersionPrefix, "novprefix", false, "Omit \"v\" version prefix")
+	flag.StringVar(&options.OutputPath, "output", "-", "Output path")
+	flag.StringVar(&options.SerialNumberStr, "serial", "", "Serial number (default [random UUID])")
+	flag.BoolVar(&options.ShowVersion, "version", false, "Show version")
+	flag.BoolVar(&options.UseJSON, "json", false, "Output in JSON format")
 	flag.Parse()
 
-	if showVersion {
+	if options.ShowVersion {
 		fmt.Println(version.Version)
 		return
 	}
 
-	if err := validateArguments(); err != nil {
+	if err := validateOptions(&options); err != nil {
 		log.Fatal(err)
 	}
 
-	parsedSerialNumber := new(uuid.UUID)
-	if serial, err := uuid.Parse(serialNumber); err == nil {
-		parsedSerialNumber = &serial
+	if err := executeCommand(options); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var allowedComponentTypes = []cdx.ComponentType{
+	cdx.ComponentTypeApplication,
+	cdx.ComponentTypeContainer,
+	cdx.ComponentTypeDevice,
+	cdx.ComponentTypeFile,
+	cdx.ComponentTypeFirmware,
+	cdx.ComponentTypeFramework,
+	cdx.ComponentTypeLibrary,
+	cdx.ComponentTypeOS,
+}
+
+func validateOptions(options *Options) error {
+	isAllowedComponentType := false
+	for i := range allowedComponentTypes {
+		if allowedComponentTypes[i] == cdx.ComponentType(options.ComponentTypeStr) {
+			isAllowedComponentType = true
+			break
+		}
+	}
+	if isAllowedComponentType {
+		options.ComponentType = cdx.ComponentType(options.ComponentTypeStr)
+	} else {
+		return fmt.Errorf("invalid component type %s. See https://cyclonedx.org/docs/1.2/#type_classification", options.ComponentTypeStr)
 	}
 
-	bom, err := sbom.Generate(modulePath, sbom.GenerateOptions{
-		ComponentType:   cdx.ComponentType(componentType),
-		IncludeStdLib:   includeStd,
-		NoSerialNumber:  noSerialNumber,
-		NoVersionPrefix: noVersionPrefix,
-		SerialNumber:    parsedSerialNumber,
+	// Serial numbers must be valid UUIDs
+	if !options.NoSerialNumber && options.SerialNumberStr != "" {
+		if serialNumber, err := uuid.Parse(options.SerialNumberStr); err != nil {
+			return fmt.Errorf("invalid serial number: %w", err)
+		} else {
+			options.SerialNumber = &serialNumber
+		}
+	}
+
+	return nil
+}
+
+func executeCommand(options Options) error {
+	bom, err := sbom.Generate(options.ModulePath, sbom.GenerateOptions{
+		ComponentType:   options.ComponentType,
+		IncludeStdLib:   options.IncludeStd,
+		NoSerialNumber:  options.NoSerialNumber,
+		NoVersionPrefix: options.NoVersionPrefix,
+		SerialNumber:    options.SerialNumber,
 	})
+	if err != nil {
+		return fmt.Errorf("generating sbom failed: %w", err)
+	}
 
 	var outputFormat cdx.BOMFileFormat
-	if useJSON {
+	if options.UseJSON {
 		outputFormat = cdx.BOMFileFormatJSON
 	} else {
 		outputFormat = cdx.BOMFileFormatXML
 	}
 
 	var outputWriter io.Writer
-	if outputPath == "" || outputPath == "-" {
+	if options.OutputPath == "" || options.OutputPath == "-" {
 		outputWriter = os.Stdout
 	} else {
-		outputFile, err := os.Create(outputPath)
+		outputFile, err := os.Create(options.OutputPath)
 		if err != nil {
-			log.Fatalf("failed to create output file %s: %v", outputPath, err)
+			return fmt.Errorf("failed to create output file %s: %w", options.OutputPath, err)
 		}
 		defer outputFile.Close()
 		outputWriter = outputFile
@@ -94,27 +128,7 @@ func main() {
 	encoder.SetPretty(true)
 
 	if err = encoder.Encode(bom); err != nil {
-		log.Fatalf("encoding BOM failed: %v", err)
-	}
-}
-
-func validateArguments() error {
-	isAllowedComponentType := false
-	for i := range allowedComponentTypes {
-		if allowedComponentTypes[i] == cdx.ComponentType(componentType) {
-			isAllowedComponentType = true
-			break
-		}
-	}
-	if !isAllowedComponentType {
-		return fmt.Errorf("invalid component type %s. See https://pkg.go.dev/github.com/CycloneDX/cyclonedx-go#ComponentType for options", componentType)
-	}
-
-	// Serial numbers must be valid UUIDs
-	if !noSerialNumber && serialNumber != "" {
-		if _, err := uuid.Parse(serialNumber); err != nil {
-			return fmt.Errorf("invalid serial number: %w", err)
-		}
+		return fmt.Errorf("encoding BOM failed: %w", err)
 	}
 
 	return nil
