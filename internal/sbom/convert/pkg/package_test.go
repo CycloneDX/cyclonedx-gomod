9e42c9b93a51b56c2aededa4577ba5de058b8f19
nscuro <nscuro@protonmail.com>
2021-11-21 18:46:46 +0100

    add test
    Signed-off-by: nscuro <nscuro@protonmail.com>

---
+++ internal/sbom/convert/pkg/package_test.go
@@ -0,0 +1,25 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/CycloneDX/cyclonedx-gomod/internal/gomod"
+	"github.com/stretchr/testify/require"
+)
+
+func TestToComponent(t *testing.T) {
+	m := gomod.Module{
+		Path:    "modulePath",
+		Version: "moduleVersion",
+	}
+
+	p := gomod.Package{
+		ImportPath: "packagePath",
+	}
+
+	c, err := ToComponent(p, m)
+	require.NoError(t, err)
+	require.Equal(t, "packagePath", c.Name)
+	require.Equal(t, "moduleVersion", c.Version)
+	require.Equal(t, "pkg:golang/packagePath@moduleVersion?type=package", c.PackageURL)
+}
