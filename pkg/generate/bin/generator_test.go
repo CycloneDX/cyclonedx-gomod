cd7d5e3fdd12bdfb59f988520aa8c4dcb9fb3871
nscuro <nscuro@protonmail.com>
2022-01-09 18:21:32 +0100

    move `bin` e2e tests to generator package
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- pkg/generate/bin/generator_test.go
+++ pkg/generate/bin/generator_test.go
@@ -17,12 +17,90 @@
 
 package bin
 
-import "testing"
+import (
+	"errors"
+	"io"
+	"runtime"
+	"testing"
+
+	"github.com/bradleyjkemp/cupaloy/v2"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
+
+	"github.com/CycloneDX/cyclonedx-gomod/internal/bomtest"
+)
+
+var (
+	silentLogger = zerolog.New(io.Discard)
+	snapShooter  = cupaloy.NewDefaultConfig().
+			WithOptions(cupaloy.SnapshotSubdirectory("./testdata/snapshots"))
+)
 
 func TestNewGenerator(t *testing.T) {
-	// TODO
+	t.Run("Success", func(t *testing.T) {
+		g, err := NewGenerator("")
+		require.NoError(t, err)
+		require.NotNil(t, g)
+	})
+
+	t.Run("OptionError", func(t *testing.T) {
+		failOption := func(g *generator) error {
+			return errors.New("test")
+		}
+
+		g, err := NewGenerator("", failOption)
+		require.Nil(t, g)
+		require.Error(t, err)
+		require.Equal(t, "test", err.Error())
+	})
 }
 
 func TestGenerator_Generate(t *testing.T) {
-	// TODO
+	t.Run("Success", func(t *testing.T) {
+		skipOnWindows(t)
+
+		g, err := NewGenerator("./testdata/simple",
+			WithLogger(silentLogger),
+			WithIncludeStdlib(true))
+		require.NoError(t, err)
+
+		bom, err := g.Generate()
+		require.NoError(t, err)
+
+		bomtest.RequireSnapshot(t, snapShooter, bom)
+		bomtest.RequireValid(t, bom)
+	})
+
+	t.Run("SuccessGo1.18", func(t *testing.T) {
+		skipOnWindows(t)
+
+		g, err := NewGenerator("./testdata/simple1.18",
+			WithLogger(silentLogger),
+			WithIncludeStdlib(true))
+		require.NoError(t, err)
+
+		bom, err := g.Generate()
+		require.NoError(t, err)
+
+		bomtest.RequireSnapshot(t, snapShooter, bom)
+		bomtest.RequireValid(t, bom)
+	})
+
+	t.Run("BinaryDoesNotExist", func(t *testing.T) {
+		g, err := NewGenerator("./testdata/doesNotExist", WithLogger(silentLogger))
+		require.NoError(t, err)
+
+		bom, err := g.Generate()
+		require.Nil(t, bom)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "failed to load build info")
+	})
+}
+
+// Go does not recognize the test binaries as executable on Windows.
+// Once we've migrated to Go 1.18's debug.BuildInfo, this can be removed.
+func skipOnWindows(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip()
+	}
 }
